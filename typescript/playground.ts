3eed70119cef724b61378a24a70c6c713887d319
Franco Berardi <darkwilly08@gmail.com>
2023-05-01 00:17:49 -0300

    feat: typescript playground ready

---
+++ typescript/playground.ts
@@ -0,0 +1,53 @@
+import * as fs from "fs";
+import promptSync from "prompt-sync";
+import { exec } from "child_process";
+
+const prompt = promptSync({ sigint: true });
+
+const getSnippets = async () => {
+  return new Promise<string[]>((resolve, reject) => {
+    fs.readdir("./snippets", (err, files) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(files);
+      }
+    });
+  });
+};
+
+const executeSelectedSnippet = async (snippetName: string) => {
+  console.log(`Running ./snippets/${snippetName}`);
+  await new Promise((resolve, reject) => {
+    exec(`ts-node-esm ./snippets/${snippetName}`, (err, stdout, stderr) => {
+      if (err) {
+        console.error('script error', err);
+        reject(err);
+      } else {
+        console.log(stdout);
+        resolve(null);
+      }
+    });
+  });
+};
+
+const start = async () => {
+  const snippets = await getSnippets();
+  snippets.forEach((snippet, index) => {
+    console.log(`[${index}] ${snippet}`);
+  });
+
+  const option = Number(prompt("Select a snippet to run: "));
+
+  if (option >= snippets.length) {
+    console.log("Invalid option");
+    return;
+  }
+
+  
+  await executeSelectedSnippet(snippets[option]);
+};
+
+start()
+  .catch(console.error)
+  .finally(() => process.exit());
---
+++ typescript/snippets/sortArray.ts
@@ -0,0 +1,6 @@
+const arr = [2,1,3];
+
+// Sorts the array in place
+arr.sort((a, b) => a - b);
+
+console.log(arr); // [1, 2, 3]
